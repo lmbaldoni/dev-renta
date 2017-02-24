da3402204fbf038bbd9f97e66a92117b8fae81d9
lmbaldoni <lmbaldoni@gmail.com>
2017-02-23 23:24:10 -0600

    componentes de angular 2 importados

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MainCompComponent } from './main-comp/main-comp.component';
+import { MenuCompComponent } from './menu-comp/menu-comp.component';
 
 @Component({
   selector: 'app-root',
