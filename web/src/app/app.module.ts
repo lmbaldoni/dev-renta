b9237340ce2f42a468051e214528c6ae46ac0e75
ofsita <ofsita@ofsitahost.ofsitadomain>
2017-02-25 14:04:21 -0600

    Bootstrap 3 con pagina principal cargada, pero sin funcionamiento

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DimensionsCompComponent } from './main-comp/data-management-comp/dimens
 import { MembersCompComponent } from './main-comp/data-management-comp/dimensions-comp/members-comp/members-comp.component';
 import { AttributesCompComponent } from './main-comp/data-management-comp/dimensions-comp/attributes-comp/attributes-comp.component';
 import { HierarchiesCompComponent } from './main-comp/data-management-comp/dimensions-comp/hierarchies-comp/hierarchies-comp.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { HierarchiesCompComponent } from './main-comp/data-management-comp/dimen
     DimensionsCompComponent,
     MembersCompComponent,
     AttributesCompComponent,
-    HierarchiesCompComponent
+    HierarchiesCompComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
