df7489ffdc71d7c838619820775bace56824b26e
lmbaldoni <lmbaldoni@gmail.com>
2017-03-12 20:35:47 -0600

    ajuste de componentes en rule allocations

--- web/src/app/main-comp/pft-comp/pft-rule-comp/pft-rule-comp.component.ts
+++ web/src/app/main-comp/pft-comp/pft-rule-comp/pft-rule-comp.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { OutputCompComponent } from './output-comp/output-comp.component';
+import { InitialRuleCompComponent } from './initial-rule-comp/initial-rule-comp.component';
+import { OperatorCompComponent } from './operator-comp/operator-comp.component';
+import { DriverCompComponent } from './driver-comp/driver-comp.component';
+import { SourceCompComponent } from './source-comp/source-comp.component';
 
 @Component({
   selector: 'app-pft-rule-comp',
