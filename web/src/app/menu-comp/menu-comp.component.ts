fb5801882eacb0493bbe15cc1b3967571bcb3730
lmbaldoni <lmbaldoni@gmail.com>
2017-03-14 20:51:35 -0600

    boton allocation view

--- web/src/app/menu-comp/menu-comp.component.ts
+++ web/src/app/menu-comp/menu-comp.component.ts
@@ -10,6 +10,10 @@ export class MenuCompComponent implements OnInit {
   constructor() { }
 
 
+/*hide show header and menu*/
+headerClicked = true;
+menuClicked = true;
+
 /*hide show DATA MANAGEMENT*/
 
   membersClicked: boolean = false;
@@ -17,9 +21,18 @@ export class MenuCompComponent implements OnInit {
   hierarchiesClicked: boolean = false;
   filtersClicked: boolean = false;
 
+  setAllDataManagement(){
+    this.membersClicked = false;
+    this.attributesClicked = false;
+    this.hierarchiesClicked = false;
+    this.filtersClicked = false;
+  }
+
   setMembersClicked(clicked: boolean) {
       this.membersClicked = clicked;
       /*ocultar*/
+      this.setAllOperations();
+      this.setAllMigration();
       this.attributesClicked = !clicked;
       this.hierarchiesClicked = !clicked;
       this.filtersClicked = !clicked;
@@ -29,6 +42,8 @@ export class MenuCompComponent implements OnInit {
   setAttributesClicked(clicked: boolean) {
       this.attributesClicked = clicked;
       /*ocultar*/
+      this.setAllOperations();
+      this.setAllMigration();
       this.membersClicked = !clicked;
       this.hierarchiesClicked = !clicked;
       this.filtersClicked = !clicked;
@@ -38,6 +53,8 @@ export class MenuCompComponent implements OnInit {
   setHierarchiesClicked(clicked: boolean) {
       this.hierarchiesClicked = clicked;
       /*ocultar*/
+      this.setAllOperations();
+      this.setAllMigration();
       this.attributesClicked = !clicked;
       this.membersClicked = !clicked;
       this.filtersClicked = !clicked;
@@ -46,6 +63,8 @@ export class MenuCompComponent implements OnInit {
    setFiltersClicked(clicked: boolean) {
       this.filtersClicked = clicked;
       /*ocultar*/
+      this.setAllOperations();
+      this.setAllMigration();
       this.attributesClicked = !clicked;
       this.membersClicked = !clicked;
       this.hierarchiesClicked = !clicked;
@@ -57,9 +76,17 @@ export class MenuCompComponent implements OnInit {
   batchExeClicked: boolean = false;
   batchMoniClicked: boolean = false;
   
+  setAllOperations(){
+    this.batchManteClicked = false;
+    this.batchExeClicked = false;
+    this.batchMoniClicked = false;
+  }
+
   setBatchManteClicked(clicked: boolean) {
       this.batchManteClicked = clicked;
       /*ocultar*/
+      this.setAllDataManagement();
+      this.setAllMigration();
       this.batchExeClicked = !clicked;
       this.batchMoniClicked = !clicked;
   }
@@ -67,6 +94,8 @@ export class MenuCompComponent implements OnInit {
   setBatchExeClicked(clicked: boolean) {
       this.batchExeClicked = clicked;
       /*ocultar*/
+      this.setAllDataManagement();
+      this.setAllMigration();
       this.batchManteClicked = !clicked;
       this.batchMoniClicked = !clicked;
   }
@@ -75,8 +104,90 @@ export class MenuCompComponent implements OnInit {
   setBatchMoniClicked(clicked: boolean) {
       this.batchMoniClicked = clicked;
       /*ocultar*/
+      this.setAllDataManagement();
+      this.setAllMigration();
       this.batchExeClicked = !clicked;
       this.batchManteClicked = !clicked;
+  }
+
+
+  /*hide show MIGRATION*/
+
+  importClicked: boolean = false;
+  exportClicked: boolean = false;
+  
+  
+  setAllMigration(){
+    this.importClicked = false;
+    this.exportClicked = false;
+  }
+
+  setImportClicked(clicked: boolean) {
+      this.importClicked = clicked;
+      /*ocultar*/
+      this.setAllDataManagement();
+      this.setAllOperations();
+      this.exportClicked = !clicked;
+      
+  }
+
+  setExportClicked(clicked: boolean) {
+      this.exportClicked = clicked;
+      /*ocultar*/
+      this.setAllDataManagement();
+      this.setAllOperations();
+      this.importClicked = !clicked;
+      
+  }
+
+
+   /*hide show PFT*/
+
+  allocationClicked: boolean = false;
+  allocationModelClicked: boolean = false;
+  allocationHistoryClicked: boolean = false;
+  
+  
+  setAllPFT(){
+    this.allocationClicked = false;
+    this.allocationModelClicked = false;
+    this.allocationHistoryClicked = false;
+  }
+
+  setAllocationClicked(clicked: boolean) {
+      /*ocultar*/
+      this.setAllDataManagement();
+      this.setAllOperations();
+      this.setAllMigration();
+      this.allocationModelClicked = !clicked;
+      this.allocationHistoryClicked = !clicked;
+      this.headerClicked = !clicked;
+      this.menuClicked = !clicked;
+      /*mostrar*/
+      this.allocationClicked = clicked;
+      
+  }
+
+  setAllocationModelClicked(clicked: boolean) {
+      this.allocationModelClicked = clicked;
+      /*ocultar*/
+      this.setAllDataManagement();
+      this.setAllOperations();
+      this.setAllMigration();
+      this.allocationClicked = !clicked;
+      this.allocationHistoryClicked = !clicked;
+      
+  }
+
+    setAllocationHistoryClicked(clicked: boolean) {
+      this.allocationHistoryClicked = clicked;
+      /*ocultar*/
+      this.setAllDataManagement();
+      this.setAllOperations();
+      this.setAllMigration();
+      this.allocationClicked = !clicked;
+      this.allocationModelClicked = !clicked;
+      
   }
 
   ngOnInit() {
