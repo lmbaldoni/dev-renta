25064edf3032c65df4fc02f5f54843a16fd48eac
lmbaldoni <HBALU820@banorte.com>
2017-05-19 18:11:33 -0500

    agrego servicio miembro

--- web/src/app/dimensions/dimensions.module.ts
+++ web/src/app/dimensions/dimensions.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { HttpModule } from '@angular/http';
 
 import { DimensionsRoutingModule } from './dimensions-routing.module';
 import { MembersComponent } from './members.component';
+import { MembersService } from './members.service';
 
 @NgModule({
   imports: [
     CommonModule,
     DimensionsRoutingModule,
     FormsModule,
+    HttpModule,
   ],
-  declarations: [ MembersComponent ]
+  declarations: [ MembersComponent ],
+  providers: [ MembersService ],
 })
 export class DimensionsModule { }
--- web/src/app/dimensions/members.component.ts
+++ web/src/app/dimensions/members.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { Member , Attribute } from './member.model';
 
+//importar libreria de servicios
+import { Http } from '@angular/http';
+import { MembersService } from './members.service';
+
 @Component({
   templateUrl: 'members.component.html'
 })
@@ -8,7 +12,7 @@ export class MembersComponent {
 
   public model = new Member(null,false,null, '', '', '',false,false,'lucas','lucas');
 
-  constructor() { }
+  constructor(private http: Http,private membersService: MembersService) { }
 
   CreateMemderbuttonWasClicked: boolean = false;
   SearchMemderbuttonWasClicked: boolean = true;
@@ -27,7 +31,30 @@ export class MembersComponent {
 
     this.model = new Member(null,false,null, '', '', '',false,false,'lucas','lucas');
 
-  } 
+  }
+
+  //Recuperar dimensiones de procesamiento
+
+  public dimensions = [];
+  public urldimensions =  "http://localhost:8000/"+"dimensions/";
+
+  private refreshDimensions() {
+     
+     this.dimensions.length = 0;
+
+    this.http.get(this.urldimensions).subscribe(
+      response => {
+        let data = response.json();
+        for (var i = 0; i < data.results.length; i++) {
+          let id_alfa = data.results[i];
+          this.dimensions.push(id_alfa);
+        }
+      },
+      error => console.error(error)
+    );
+
+
+   }
 
 }
 
